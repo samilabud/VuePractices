7a6b5d0bbb71ac12eaddaa0074d443b8e101cfbd
Samil Abud <samilabud@gmail.com>
2024-02-21 22:25:46 -0400

    Added counter component

--- Tutorial/src/App.vue
+++ Tutorial/src/App.vue
@@ -1,17 +1,13 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App" />
+  <CounterComponent />
 </template>
 
-<script>
+<script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import CounterComponent from './components/CounterComponent.vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
 <style>
---
+++ Tutorial/src/components/CounterComponent.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue'
+
+const counter = ref(0);
+
+const increase = () => {
+    counter.value++
+}
+
+</script>
+
+<template>
+    <p><span>{{ counter }}</span></p>
+    <button @click="increase">Increase!</button>
+</template>
