b24c6d2c416e9305d8c6f946039375e79177da6d
Samil Abud <samilabud@gmail.com>
2024-02-25 22:34:20 -0400

    Added ref model to each form control

--- Tutorial/src/components/ContactForm.vue
+++ Tutorial/src/components/ContactForm.vue
@@ -13,48 +13,49 @@
                         <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="first-name">
                             First name
                         </label>
-                        <input class="input" id="first-name" placeholder="Enter your first name" />
+                        <input class="input" id="first-name" placeholder="Enter your first name" v-model="firstName" />
                     </div>
                     <div class="space-y-2">
                         <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="last-name">
                             Last name
                         </label>
-                        <input class="input" id="last-name" placeholder="Enter your last name" />
+                        <input class="input" id="last-name" placeholder="Enter your last name" v-model="lastName" />
                     </div>
                 </div>
                 <div class="space-y-2">
                     <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="email">
                         Email
                     </label>
-                    <input class="fullinput" id="email" placeholder="Enter your email" type="email" />
+                    <input class="fullinput" id="email" placeholder="Enter your email" type="email" v-model="email" />
                 </div>
                 <div class="space-y-2">
                     <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="message">
                         Message
                     </label>
-                    <textarea class="fullinput min-h-[100px]" id="message" placeholder="Enter your message"></textarea>
+                    <textarea class="fullinput min-h-[100px]" id="message" placeholder="Enter your message"
+                        v-model="message"></textarea>
                 </div>
                 <div class="space-y-2">
                     <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="message">
                         Favorite Frontend Language
                     </label>
                     <div class="flex justify-between w-28">
-                        <input type="radio" name="language" value="Vue" id="vue" />
+                        <input type="radio" name="language" value="Vue" id="vue" v-model="language" />
                         <label class="block tracking-wide text-gray-700 text-xs font-bold" for="vue">
                             Vue
                         </label>
-                        <input type="radio" name="language" value="React" id="react" />
+                        <input type="radio" name="language" value="React" id="react" v-model="language" />
                         <label class="block tracking-wide text-gray-700 text-xs font-bold" for="react">
                             React
                         </label>
                     </div>
                 </div>
                 <div class="space-y-2">
-                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="message">
+                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="places">
                         Favorite Place in DR
                     </label>
                     <div class="flex justify-between">
-                        <select name="places" class="selectinput">
+                        <select name="places" class="selectinput" id="places" v-model="places">
                             <option value="" disabled selected>Select a place</option>
                             <option>Constanza</option>
                             <option>Samaná</option>
@@ -65,19 +66,19 @@
                 </div>
                 <div class="space-y-2">
                     <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="message">
-                        Favorites Fruits
+                        Favorite Fruits
                     </label>
                     <div class="flex justify-between px-3">
-                        <input type="checkbox" id="strawberry" value="Strawberry" />
+                        <input type="checkbox" id="strawberry" value="Strawberry" v-model="fruits" />
                         <label for="strawberry">Strawberry</label>
 
-                        <input type="checkbox" id="apple" value="Apple" />
+                        <input type="checkbox" id="apple" value="Apple" v-model="fruits" />
                         <label for="apple">Apple</label>
 
-                        <input type="checkbox" id="grape" value="Grape" />
+                        <input type="checkbox" id="grape" value="Grape" v-model="fruits" />
                         <label for="grape">Grape</label>
 
-                        <input type="checkbox" id="blueberry" value="Blueberry" />
+                        <input type="checkbox" id="blueberry" value="Blueberry" v-model="fruits" />
                         <label for="blueberry">Blueberry</label>
                     </div>
                 </div>
@@ -89,8 +90,37 @@
             </button>
         </div>
     </div>
+    <div class="rounded-lg border bg-gray-100 text-black shadow-sm w-full max-w-md mx-auto">
+        <div v-if="firstName || lastName">
+            <stong>Name:</stong> <span>{{ firstName }} {{ lastName }}</span>
+        </div>
+        <div v-if="email">
+            <strong>Email:</strong><span>{{ email }}</span>
+        </div>
+        <div v-if="message">
+            <strong>Message:</strong>
+            <p style="white-space: pre-line;">{{ message }}</p>
+        </div>
+        <div v-if="language">
+            <strong>Favorite language:</strong><span>{{ language }}</span>
+        </div>
+        <div v-if="places">
+            <strong>Favorite place:</strong><span>{{ places }}</span>
+        </div>
+        <div v-if="fruits.length > 0">
+            <strong>Favorite fruits: </strong><span>{{ fruits.join(',') }}</span>
+        </div>
+    </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
 
+const firstName = ref('');
+const lastName = ref('')
+const email = ref('')
+const message = ref('');
+const language = ref('')
+const places = ref('')
+const fruits = ref([])
 </script>
