47d2996fe4e8a7b09b97ff7ced122feb258db030
Samil Abud <samilabud@gmail.com>
2024-02-27 21:03:01 -0400

    Added banners to show error or success message when form be submited

--- Tutorial/src/components/ContactForm.vue
+++ Tutorial/src/components/ContactForm.vue
@@ -96,6 +96,27 @@
                     Send message
                 </button>
             </div>
+            <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert"
+                v-if="hasError && formStatusMessage">
+                <strong class="font-bold">Holy smokes! </strong>
+                <span class="block sm:inline">{{ formStatusMessage }}</span>
+                <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                    <svg class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20">
+                        <title>Close</title>
+                        <path
+                            d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+                    </svg>
+                </span>
+            </div>
+            <div class="flex items-center bg-blue-500 text-white text-sm font-bold px-4 py-3" role="alert"
+                v-if="!hasError && formStatusMessage">
+                <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                    <path
+                        d="M12.432 0c1.34 0 2.01.912 2.01 1.957 0 1.305-1.164 2.512-2.679 2.512-1.269 0-2.009-.75-1.974-1.99C9.789 1.436 10.67 0 12.432 0zM8.309 20c-1.058 0-1.833-.652-1.093-3.524l1.214-5.092c.211-.814.246-1.141 0-1.141-.317 0-1.689.562-2.502 1.117l-.528-.88c2.572-2.186 5.531-3.467 6.801-3.467 1.057 0 1.233 1.273.705 3.23l-1.391 5.352c-.246.945-.141 1.271.106 1.271.317 0 1.357-.392 2.379-1.207l.6.814C12.098 19.02 9.365 20 8.309 20z" />
+                </svg>
+                <p>{{ formStatusMessage }}</p>
+            </div>
         </div>
     </form>
     <p class="text-center text-gray-500 text-xs">
@@ -138,6 +159,9 @@ const fruits = ref([])
 
 const firstNameError = ref('')
 
+const hasError = ref(false);
+const formStatusMessage = ref("");
+
 const runValidations = () => {
     firstNameError.value = firstName.value === "" ? "Should has a first name!" : "";
 }
@@ -146,11 +170,13 @@ watch(firstName, () => {
 })
 
 const handleSubmit = () => {
+    hasError.value = false
     runValidations();
     if (!firstNameError.value) {
-        alert("Form submited!")
+        formStatusMessage.value = "Form submited!";
     } else {
-        alert("Should fix errors!")
+        hasError.value = true;
+        formStatusMessage.value = "Should fix errors!";
     }
 }
 
